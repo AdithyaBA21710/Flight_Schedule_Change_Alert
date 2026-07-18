5d8eabd19e583c3875cbb17e313b5b58f0cac86e
AdithyaBA21710 <adithya.ba21710@gmail.com>
2026-07-18 23:48:08 +0530

    Addded dependencies for Timer Trigger function

--- ScheduleSearch v2 (TT)/function_app.py
+++ ScheduleSearch v2 (TT)/function_app.py
@@ -1,5 +1,10 @@
+import os
+import json
 import logging
 import azure.functions as func
+from azure.communication.email import EmailClient
+from azure.core.credentials import AzureKeyCredential
+from azure.storage.blob import BlobServiceClient
 
 app = func.FunctionApp()
 
